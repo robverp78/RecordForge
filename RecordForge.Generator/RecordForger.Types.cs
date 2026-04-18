1238dd9ba330a7c89eb385aff0415f35c1892b62
robverp78 <robverpa@gmail.com>
2026-04-18 21:12:21 +0200

    Created separate repo for RecordForge

---
+++ RecordForge.Generator/RecordForger.Types.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Subro.RecordForge.Generator.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subro.Generators
+{
+
+    partial class RecordForger
+    {
+
+
+        /// <summary>
+        /// All available options of <see cref="RecordKind"/>, expressed through <see cref="GenerateTypeInfo"/>. 
+        /// This is used to convert from the enum value to the needed information for generation
+        /// </summary>
+        internal static GenerateTypeInfo[] generateTypeInfos =
+           [
+                new( (int)RecordKind.Record, "record", true),
+                new( (int)RecordKind.RecordStruct, "record struct", true),
+                new((int)RecordKind.ReadOnlyRecordStruct, "record struct", true, true ),
+                new( (int)RecordKind.Struct, "struct", false),
+                new((int)RecordKind.ReadonlyStruct, "struct", false,true),
+                new((int)RecordKind.Class, "class", false),
+           ];
+
+
+        internal static GenerateTypeInfo? GetGenerateTypeInfo(AttributeData attributeData, int Index)
+        {
+            var kind  = attributeData.ConstructorArguments.Length > Index
+                    && attributeData.ConstructorArguments[Index].Value is int kindValue
+                        ? kindValue
+                        : (int)GenerateRecordAttributeBase.DefaultKind;
+            return GetGenerateTypeInfo(kind);
+        }
+
+        internal static GenerateTypeInfo? GetGenerateTypeInfo(int kind)
+            => generateTypeInfos.FirstOrDefault(info => info.KindValue == kind);
+        internal static DiagnosticInfoProvider CreateInvalidKindDiagnostic(AttributeData attributeData)
+            => Diagnostics.Error("AIRINVKIND", "Invalid value for " + nameof(RecordKind))
+                    .CreateDiagnosticInfo(LocationInfo.From(attributeData));
+    }
+
+    /// <summary>
+    /// Information on what a <see cref="RecordKind"/> entails, and how to convert it
+    /// </summary>
+    internal class GenerateTypeInfo(int KindValue, string Keywords, bool OnlyNeedsConstructor, bool IsReadOnly = false)
+    {
+        public readonly int KindValue = KindValue;
+        public readonly string Keywords = Keywords;
+        public readonly bool OnlyNeedsConstructor = OnlyNeedsConstructor;
+        public readonly bool IsReadOnly = IsReadOnly;
+    }
+
+    record CreationSettings
+    {
+        public bool AsPartial { get; set; } = GenerateRecordAttribute.DefaultAsPartial;
+
+        public bool AsAbstract { get; set; } = false;
+        public bool AlwaysCreateSetters { get; set; }
+
+        public CtorUsage ConstructorUsage { get; set; }
+    }
+
+    [Flags]
+    enum CtorUsage
+    {
+        Automatic = 0,
+        Empty = ConstructorUsage.Empty,
+        ReadonlyProperties = ConstructorUsage.ReadonlyProperties,
+        ReadonlyAndInitProperties = ConstructorUsage.ReadonlyAndInitProperties,
+        AllProperties = ConstructorUsage.AllProperties,
+    }
+
+    record CreationInfo(
+        GenerateTypeInfo TypeInfo,
+        string InterfaceName,
+        string InterfaceNameSpace,
+        string RecordName,
+        string RecordNameSpace,
+        EquatableArray<PropertyCreationInfo> Properties,
+        CreationSettings Settings)
+    {
+        /*
+         * In loving memory of Smokey. 
+         * Smokes, you were such an awesome kitty cat. You were my buddy.
+         * I miss your presence and I miss you jumping on my desk while I was coding. 
+         * I miss it even more how you crawled on my chest to get your cuddles.
+         * When I was working on the last bits of this generator, you were too sick to still do that,
+         * but we never imagined those would be the last days we would have you in our lives.
+         */
+
+
+        public IEnumerable<CtorUsage> GetConstructorTypes()
+        {
+            var ctors = Settings.ConstructorUsage;
+            if (ctors == CtorUsage.Automatic)
+            {
+                //Default value is automatic, behavior should correspond with  ConstructorUsage.Automatic description
+                if (TypeInfo.IsReadOnly)
+                    yield return CtorUsage.AllProperties;
+                else if (TypeInfo.OnlyNeedsConstructor)
+                    yield return CtorUsage.ReadonlyProperties;
+                else
+                    yield return CtorUsage.Empty;
+            }
+            else if ((ctors & (ctors - 1)) == 0) // single flag, common case
+                yield return ctors;
+            else
+            {
+                //multiple flags — check each one
+                if (ctors.HasFlag(CtorUsage.Empty)) yield return CtorUsage.Empty;
+                if (ctors.HasFlag(CtorUsage.ReadonlyProperties)) yield return CtorUsage.ReadonlyProperties;
+                if (ctors.HasFlag(CtorUsage.ReadonlyAndInitProperties)) yield return CtorUsage.ReadonlyAndInitProperties; 
+                if (ctors.HasFlag(CtorUsage.AllProperties)) yield return CtorUsage.AllProperties;
+            }
+        }
+
+        public IEnumerable<IReadOnlyList<PropertyCreationInfo>> GetConstructors()
+        {
+            foreach (var ctor in GetConstructorTypes())
+                yield return ctor switch
+                {
+                    CtorUsage.Empty => [],
+                    CtorUsage.ReadonlyProperties => [.. Properties.Where(static p => !p.HasSetter)],
+                    CtorUsage.ReadonlyAndInitProperties => [.. Properties.Where(static p => p.HasInit || !p.HasSetter)],
+                    CtorUsage.AllProperties => Properties,
+                    _ => throw new InvalidOperationException("Invalid constructor usage")
+                };
+        }
+
+        public bool IsReadOnly => TypeInfo.IsReadOnly;
+        public bool IsPartial => Settings.AsPartial;
+    }
+
+    internal class PropertyReference
+    {
+        public required PropertyCreationInfo Property{ get; init; }
+        public bool IsInPrimaryCtor { get; set; }
+
+        public string Name => Property.Name;
+    }
+
+    internal record PropertyCreationInfo : PropertyInfo
+    {
+        public PropertyCreationInfo(IPropertySymbol prop) : base(prop)
+        {
+            HasInit = prop.SetMethod?.IsInitOnly == true;
+        }
+
+        /// <summary>
+        /// Syntax-only constructor: extracts property info from syntax without needing a semantic model.
+        /// Type names are taken as-written (not fully qualified).
+        /// </summary>
+        public PropertyCreationInfo(PropertyDeclarationSyntax syntax)
+            : base(
+                syntax.Identifier.Text,
+                new TypeReference(syntax.Type.ToString(), syntax.Type.ToString()),
+                syntax.AccessorList?.Accessors.Any(a => a.IsKind(SyntaxKind.GetAccessorDeclaration)) == true
+                    || syntax.ExpressionBody != null,
+                syntax.AccessorList?.Accessors.Any(a => a.IsKind(SyntaxKind.SetAccessorDeclaration)) == true)
+        {
+            HasInit = syntax.AccessorList?.Accessors.Any(a => a.IsKind(SyntaxKind.InitAccessorDeclaration)) == true;
+        }
+
+        public readonly bool HasInit;
+
+    }
+}
---
+++ RecordForge.Tests/RecordForge.Tests.cs
@@ -0,0 +1,93 @@
+﻿using Subro.RecordForge;
+using Subro.Generators;
+
+using Xunit;
+using static Subro.Generators.RecordForger;
+
+namespace Subro.Generators.Tests
+{
+    public class TestAutoImplementedRecords
+    {
+        TestCompiler GetTestCompilation()
+        {
+            var res = new TestCompiler();
+            res.IncrementalGenerators.Add(new RecordForger());
+            res.AdditionalReferences.Add(
+                Microsoft.CodeAnalysis.MetadataReference.CreateFromFile(
+                    typeof(GenerateRecordAttribute).Assembly.Location));
+            return res;
+        }
+
+
+        const string testInterface =
+        "interface IBar { int AnInt { get; } string AString{ get; set; } DateTime? SomeDate{get;init;} }";
+
+        static string interfaceInNamespace(string Interface)
+            => $"using Subro.RecordForge;using System; namespace Foo{{ {Interface} }}";
+        
+        [Fact]
+        public void TestRecordsAreCreatedForAllRecordKinds()
+        {
+            foreach (var kind in Enum.GetValues<RecordKind>())
+            {
+                TestRecordKind(kind);
+            }
+        }
+
+        internal void TestRecordKind(RecordKind kind)
+        {
+          var comp = GetTestCompilation().Create(interfaceInNamespace($"[GenerateRecord({nameof(RecordKind)}.{kind})]{testInterface}" )); 
+                Assert.NotNull(comp.GetSymbol("Foo.Bar"));
+        }
+
+        [Fact]
+        public void TestCreatedRecordIsUsable()
+        {
+            var comp = GetTestCompilation().Create(interfaceInNamespace($"[GenerateRecord]{testInterface} class AClass{{public AClass(){{var rec = new Bar(1);}}}}"));
+        }
+
+        [Fact]
+        public void TestUseOtherNamespaceAndName()
+        {
+            var comp = GetTestCompilation().Create(interfaceInNamespace($"[GenerateRecord(NameSpace=\"Test\", RecordName=\"Bar2\")]{testInterface}"));
+            Assert.NotNull(comp.GetSymbol("Test.Bar2"));
+        }
+ 
+        [Fact]
+        public void TestAssemblyLevelDeclaration()
+        {
+            var code = @$"
+using Subro.RecordForge;
+using System; 
+
+[assembly:GenerateRecordFromInterface(typeof(Foo.IBar))]
+[assembly:GenerateRecordFromInterface(typeof(Foo.IBar),RecordName = ""Bar2"")]
+namespace Foo{{ {testInterface} }}";
+            var comp = GetTestCompilation().Create(code );
+            Assert.NotNull(comp.GetSymbol("Foo.Bar"));
+            Assert.NotNull(comp.GetSymbol("Foo.Bar2"));
+        }
+
+        [Fact]
+        public void TestAssemblyLevelDeclarationCrossAssembly()
+        {
+            // Compile the interface in a separate assembly
+            var interfaceAssembly = new TestCompiler { AssemblyName = "InterfaceLib" }
+                .Create($"using System; namespace Foo{{ public {testInterface} }}");
+
+            // Compile the assembly-level attribute in a different assembly that references the interface assembly
+            var compiler = GetTestCompilation();
+            compiler.AdditionalReferences.Add(interfaceAssembly.ToMetadataReference());
+            var comp = compiler.Create(@"
+using Subro.RecordForge;
+[assembly:GenerateRecordFromInterface(typeof(Foo.IBar))]
+");
+            Assert.NotNull(comp.GetSymbol("Foo.Bar"));
+        }
+
+    }
+
+}
+
+
+
